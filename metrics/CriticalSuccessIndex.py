e0e4e31289899a08b68cec4997e1a9c238761fe8
sonderlau <sonderlau@foxmail.com>
2026-02-02 14:45:26 +0800

    ✨ feat(metrics/CriticalSuccessIndex.py): add pooling size parameter to CriticalSuccessIndex and CriticalSuccessIndexMean classes

--- metrics/CriticalSuccessIndex.py
+++ metrics/CriticalSuccessIndex.py
@@ -1,21 +1,33 @@
 from typing import Tuple
 
 import torch
+from torch.nn import AvgPool2d
 from torchmetrics import Metric
 
 
 class CriticalSuccessIndex(Metric):
 
-    def __init__(self, threshold: float):
+    def __init__(self, threshold: float, pooling_size: int = 1):
         super().__init__()
 
         self.threshold = threshold
+        self.pooling = pooling_size
+
+        assert pooling_size > 0, "Pooling size must be an integer and greater than 0."
+
+        if self.pooling > 1:
+            self.avg_pool = AvgPool2d(kernel_size=pooling_size, stride=1, padding=1)
 
         self.add_state("tp", default=torch.tensor(0), dist_reduce_fx="sum")
         self.add_state("fp", default=torch.tensor(0), dist_reduce_fx="sum")
         self.add_state("fn", default=torch.tensor(0), dist_reduce_fx="sum")
 
     def update(self, pred: torch.Tensor, target: torch.Tensor):
+
+        if self.pooling > 1:
+            pred = self.avg_pool(pred)
+            target = self.avg_pool(target)
+
 
         pred = (pred >= self.threshold).float()
         target = (target >= self.threshold).float()
@@ -35,16 +47,27 @@ class CriticalSuccessIndex(Metric):
 
 
 class CriticalSuccessIndexMean(Metric):
-    def __init__(self, thresholds: Tuple[float, ...]):
+    def __init__(self, thresholds: Tuple[float, ...], pooling_size: int = 1):
         super().__init__()
 
         self.thresholds = thresholds
+        self.pooling = pooling_size
+
+        assert pooling_size > 0, "Pooling size must be an integer and greater than 0."
+
+        if self.pooling > 1:
+            self.avg_pool = AvgPool2d(kernel_size=pooling_size, stride=1, padding=1)
+
 
         self.add_state("tp", default=torch.zeros(len(thresholds)), dist_reduce_fx="sum")
         self.add_state("fp", default=torch.zeros(len(thresholds)), dist_reduce_fx="sum")
         self.add_state("fn", default=torch.zeros(len(thresholds)), dist_reduce_fx="sum")
 
     def update(self, pred: torch.Tensor, target: torch.Tensor):
+        if self.pooling > 1:
+            pred = self.avg_pool(pred)
+            target = self.avg_pool(target)
+
         for i, thresh in enumerate(self.thresholds):
             p = (pred >= thresh).float()
             t = (target >= thresh).float()
